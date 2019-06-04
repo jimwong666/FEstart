e329068a54d25e6630cd5f209ff9d297adae0d18
jim <798165604@qq.com>
2019-06-04 10:00:33 +0800

    fix

--- FEknowledge/FE_Modular/src/CMD-seajs/js/entry.js
+++ FEknowledge/FE_Modular/src/CMD-seajs/js/entry.js
@@ -1,6 +1,6 @@
 // seajs 配置
 
-seajs.use(['../js/module_2'],function(data) { // 变态的地方~~
+seajs.use(['../js/module_2'],function(data) { // 变态的地方~~注意路径
 	var p = document.getElementsByTagName('p')[0];
 	p.innerHTML = "color:" + data.color +"---size:" + data.size;
 });
