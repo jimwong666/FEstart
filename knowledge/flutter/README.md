e5f7b4a3604bbea333ec6aeeec62ba25967440eb
jim <798165604@qq.com>
2019-11-29 16:32:44 +0800

    add

--- knowledge/flutter/README.md
+++ knowledge/flutter/README.md
@@ -22,10 +22,21 @@
   * build.gradle -> ext.kotlin_version = 'x.x.xx' 和 classpath 'com.android.tools.build:gradle:x.x.x'
 
 
+## API
+> 各种API:
+> Widget、MaterialApp、Scaffold、Container、Text、Image、ClipOval、ListView...
+1. <Widget>：先要自定义组件，那他们肯定都是继承自 1️⃣ StatelessWidget：无状态组件，状态不可变。2️⃣ StatefulWidget：有状态组件，持有的状态可能在widget生命周期改变。
+2. <MaterialApp>：它封装了应用程序实现Material Design所需要的一个Widget。其中包括一些常用的属性：home、title、color、theme、routes...
+3. <Scaffold>：Scaffold是Material Design布局机构的基本实现，此类提供了用于显示drawer（抽屉，比如：左边栏）、snackbar（通知，）和bottom sheet（底部按钮）的API 一般和MaterialApp一起使用。
+4. <Container>：Container是一个容器组件也叫布局组件(可以理解为前端的div，块级元素)，负责布局、绘画、定位和大小。
+| 属性名 | 类型 | 说明 |
+| :-: | :-: | :-: |
+| key | Key | Container唯一表示符，用于查找更新 |
 
 
 
 ## 技术相关
 
 ##### 路由
-1. onGenerateRoute 路路由拦截 使用时，不管是用 if语句 还是 switch语句，记得一定要加 else 和 default。否则会报错！
+
+1. onGenerateRoute 路由拦截 使用时，不管是用 if语句 还是 switch语句，记得一定要加 else 和 default。否则会报错！
---
+++ knowledge/flutter/route/demo.dart
@@ -0,0 +1,155 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Navigation with Arguments',
+      home: HomeScreen(),
+      // 这里的路由应用还是建议使用 Accept Arguments 这种形式的命名路由，简单易用，这里也是这么用的，参数全在 onGenerateRoute 这里进行判断，绑定等等，
+      // Extract Arguments 这种形式还要在子组件里进行 push，然后进行 MaterialPageRoute 的 builder、setting等等
+      onGenerateRoute: (settings) { // 这里的setting就是传参过来的所有东西，包括 context,routeName,arguments这些
+        final ScreenArguments args = settings.arguments;
+        return MaterialPageRoute<void>(
+          builder: (BuildContext context) {
+            switch(settings.name) {
+              case PassArgumentsScreen.routeName:
+                return PassArgumentsScreen(
+                  title: args.title,
+                  message: args.message,
+                );
+              default:
+                return null;
+            }
+          }
+        );
+      },
+    );
+  }
+}
+
+class HomeScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Home Screen'),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: <Widget>[
+            // A button that navigates to a named route that. The named route
+            // extracts the arguments by itself.
+            RaisedButton(
+              child: Text("Navigate to screen that extracts arguments"),
+              onPressed: () {
+                // When the user taps the button, navigate to the specific route
+                // and provide the arguments as part of the RouteSettings.
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => ExtractArgumentsScreen(),
+                    // Pass the arguments as part of the RouteSettings. The
+                    // ExtractArgumentScreen reads the arguments from these
+                    // settings.
+                    settings: RouteSettings(
+                      arguments: ScreenArguments(
+                        'Extract Arguments Screen',
+                        'This message is extracted in the build method.',
+                      ),
+                    ),
+                  ),
+                );
+              },
+            ),
+            // A button that navigates to a named route. For this route, extract
+            // the arguments in the onGenerateRoute function and pass them
+            // to the screen.
+            RaisedButton(
+              child: Text("Navigate to a named that accepts arguments"),
+              onPressed: () {
+                // When the user taps the button, navigate to a named route
+                // and provide the arguments as an optional parameter.
+                Navigator.pushNamed(
+                  context,
+                  PassArgumentsScreen.routeName,
+                  arguments: ScreenArguments(
+                    'Accept Arguments Screen',
+                    'This message is extracted in the onGenerateRoute function.',
+                  ),
+                );
+              },
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+// A Widget that extracts the necessary arguments from the ModalRoute.
+class ExtractArgumentsScreen extends StatelessWidget {
+  static const routeName = '/extractArguments';
+
+  @override
+  Widget build(BuildContext context) {
+    // Extract the arguments from the current ModalRoute settings and cast
+    // them as ScreenArguments.
+    final ScreenArguments args = ModalRoute.of(context).settings.arguments;
+
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(args.title),
+      ),
+      body: Center(
+        child: Text(args.message),
+      ),
+    );
+  }
+}
+
+// A Widget that accepts the necessary arguments via the constructor.
+class PassArgumentsScreen extends StatelessWidget {
+  const PassArgumentsScreen({
+    Key key,
+    @required this.title,
+    @required this.message,
+  }) : super(key: key);
+
+  final String title;
+  final String message;
+
+  static const routeName = '/passArguments';
+
+  // This Widget accepts the arguments as constructor parameters. It does not
+  // extract the arguments from the ModalRoute.
+  //
+  // The arguments are extracted by the onGenerateRoute function provided to the
+  // MaterialApp widget.
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(title),
+      ),
+      body: Center(
+        child: Text(message),
+      ),
+    );
+  }
+}
+
+// You can pass any object to the arguments parameter. In this example,
+// create a class that contains both a customizable title and message.
+class ScreenArguments {
+  ScreenArguments(this.title, this.message);
+
+  final String title;
+  final String message;
+}
